98583428bc3e4fa520dfbf147156aeedb1130a0a
andrei <andrei@mageda.com.br>
2020-07-13 05:03:01 -0300

    Wrap basic info in Component

---
+++ src/components/PokemonInfo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { View } from 'react-native';
+
+import Badge from './Badge';
+import Text from './Text';
+
+const PokemonInfo = prop => {
+  const { id, name, types } = useSelector(state => state?.pokemons[prop.name]);
+  const NAME = { color: 'white', fs: 26, mb: 5, bold: true, capitalize: true };
+
+  return (
+    <View>
+      <Text pokeId>#{id}</Text>
+      <Text {...NAME}>{name}</Text>
+      {types?.length && <Badge data={types} />}
+    </View>
+  );
+};
+
+export default PokemonInfo;
