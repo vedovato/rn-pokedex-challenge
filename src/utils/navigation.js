38f819b12051cdd0181b7cc19602eb788f173367
andrei <andrei@mageda.com.br>
2020-07-13 05:02:04 -0300

    update navigation methods

--- src/utils/navigation.js
+++ src/utils/navigation.js
@@ -3,27 +3,40 @@ import { Navigation } from 'react-native-navigation';
 export const defaultStyling = () => {
   Navigation.setDefaultOptions({
     statusBar: {
-      backgroundColor: 'cyan',
+      backgroundColor: 'white',
+      style: 'dark',
     },
+
     topBar: {
+      background: { color: 'transparent' },
+      title: { color: 'white', alignment: 'center' },
       backButton: { color: 'blue' },
-      visible: false,
+
+      drawBehind: false,
+      elevation: 0, // android
+      noBorder: true, // ios
     },
   });
 };
 
 export const startStackNavigation = () => {
-  const children = [{ component: { name: 'pokedex.index' } }];
+  const children = [{ component: { name: 'pokedex.list' } }];
 
   Navigation.setRoot({
     root: {
-      stack: { children },
+      stack: {
+        children,
+        options: {
+          topBar: { visible: false },
+        },
+      },
     },
   });
 };
 
-export const pushScreen = (cId, name, options = {}) => {
-  Navigation.push(cId, {
-    component: { name, options },
+export const pushScreen = props => {
+  const { componentId, name, options, passProps } = props;
+  Navigation.push(componentId, {
+    component: { name, options, passProps },
   });
 };
