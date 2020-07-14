499320fe2ebaf12916c6accdb8a6c532884038e7
andrei <andrei@mageda.com.br>
2020-07-14 00:48:15 -0300

    remove About screen

--- src/utils/screens.js
+++ src/utils/screens.js
@@ -3,28 +3,22 @@ import { Navigation } from 'react-native-navigation';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 
-import About from '../screens/About';
-
 import PokemonList from '../screens/Pokemons';
 import PokemonSingle from '../screens/Pokemon';
 
 const SCREENS = [
-  { name: 'pokedex.about', Component: About },
-
   { name: 'pokedex.list', Component: PokemonList },
   { name: 'pokedex.single', Component: PokemonSingle },
 ];
 
 const screens = () => {
-  SCREENS.map(({ name, Component }) => {
-    // const cb = props => <Component {...props} />;
-    // Navigation.registerComponent(name, () => cb());
+  SCREENS.map(({ name, Component }) =>
     Navigation.registerComponent(name, () => props => (
       <Provider store={store}>
         <Component {...props} />
       </Provider>
-    ));
-  });
+    ))
+  );
 };
 
 export default screens;
