006d42272cfcf41b0f89bbb5de763b0a696025f5
andrei <andrei@mageda.com.br>
2020-07-14 00:47:56 -0300

    refactory api method to support full URL PATH

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,8 @@
-const API_URI = 'https://pokeapi.co/api/v2';
+const API_URI = 'https://pokeapi.co/api/v2/pokemon';
 
-//FIXME: Update parameter API. Maybe an object would fit better
-const pokeApi = async (path, params = '', takeFullUrl = false) => {
-  const URI = !takeFullUrl
-    ? `${API_URI}/${path + params}` // compose url
-    : path + params; // uses path as full url
-
-  const raw = await fetch(URI);
-  const data = await raw.json();
-  return data;
+const pokeApi = async (path = API_URI) => {
+  const raw = await fetch(path);
+  return await raw.json();
 };
 
 export default pokeApi;
