5b5b09f92bb4d9bc64cd026c01dc1cf578552c71
andrei <andrei@mageda.com.br>
2020-07-14 00:46:52 -0300

    add filter component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { TextInput } from 'react-native';
 import styled from 'styled-components';
 
-const FilterFlatList = () => {
-  const [value, setValue] = useState('What Pokémon are you looking for?');
-
-  return <StyledInput onChangeText={setValue} value={value} />;
-};
+const FilterFlatList = ({ onChangeText, value }) => (
+  <StyledInput
+    placeholder="What Pokémon are you looking for?"
+    onChangeText={onChangeText}
+    value={value}
+  />
+);
 
 const StyledInput = styled(TextInput)`
-  border-radius: 10px;
-  background: #f2f2f2;
-  text-align: center;
   font-family: SF-Pro-Display-Regular;
   font-size: 16px;
+  background: #f2f2f2;
+  padding: 15px;
+  border-radius: 10px;
   color: #747476;
   margin: 20px 0;
 `;
